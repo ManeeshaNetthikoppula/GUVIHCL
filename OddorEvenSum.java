9c9dab4061bafab5924f23792f2262fdaa617df2
ManeeshaNetthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2025-12-28 10:47:15 +0530

    Add files via upload

---
+++ OddorEvenSum.java
@@ -0,0 +1,15 @@
+public class OddorEvenSum {
+    public static void main(String[] args){
+        int M = 5;
+        int N = 8;
+        int sum = N + M ;
+        if(sum % 2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
+    
+
