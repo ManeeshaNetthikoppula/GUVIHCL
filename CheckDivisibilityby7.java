b4ccb9cec0d4f3ea5ea949a1bd56c4b21067ad3b
ManeeshaNetthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2025-12-28 10:48:15 +0530

    Add files via upload

---
+++ CheckDivisibilityby7.java
@@ -0,0 +1,13 @@
+public class CheckDivisibilityby7 {
+    public static void main(String[] args) {
+        long n = 36;
+        if (n % 7 == 0){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+    }
+}
+
+
