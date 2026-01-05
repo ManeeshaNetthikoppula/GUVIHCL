d014c76d4c5b442ff213ca2fd1f6bff49dc33ac1
ManeeshaNetthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-01-05 21:20:50 +0530

    ......

---
+++ Substringcheck2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Substringcheck2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        if(str1.contains(str2)){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no" );
+        }
+    }
+    
+}
