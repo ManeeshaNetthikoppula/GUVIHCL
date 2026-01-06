bf7a4ba85b31c86b639a5aa329e206e64fa6bce0
ManeeshaNetthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-01-06 21:12:50 +0530

    ....

---
+++ BitwiseORofanElement.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BitwiseORofanElement {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int result = 0;
+        for(int i = 0; i < N; i++){
+            result |= sc.nextInt();
+        }
+        System.out.println(result);
+    }
+    
+}
