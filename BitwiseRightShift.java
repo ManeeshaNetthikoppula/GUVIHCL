9254066db71f8afe22c4e4cfdc17b3dd6c137cc3
ManeeshaNetthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-01-07 21:27:14 +0530

    ......

---
+++ BitwiseRightShift.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BitwiseRightShift {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int result = K >> N;
+        System.out.println(result);
+    }
+}
