c93be8356ebeef81c3c6e957265c452a1a08329a
ManeeshaNetthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-01-08 08:40:03 +0530

    sum sum

---
+++ SumoffirstandLastDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumoffirstandLastDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int lastDigit = n % 10;
+        int firstDigit = n;
+        while (firstDigit >= 10) {
+            firstDigit /= 10;
+        }
+        System.out.println(firstDigit + lastDigit);
+    }
+
+}
