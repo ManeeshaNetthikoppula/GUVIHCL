a3b7b34ef058d5f5711e6df28518a329ed663c21
Maneesha Netthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-01-19 11:10:30 +0530

    AlternatingArray

---
+++ AlternatingArrayCheck.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class AlternatingArrayCheck {
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+    
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean isAlternating = true;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (i % 2 == 0) {
+                
+                if (arr[i] >= arr[i + 1]) {
+                    isAlternating = false;
+                    break;
+                }
+            } else {
+                
+                if (arr[i] <= arr[i + 1]) {
+                    isAlternating = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(isAlternating ? "yes" : "no");
+        sc.close();
+    }
+    
+}
