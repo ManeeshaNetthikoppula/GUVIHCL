35f52221c163517da11aa9813d9ad01728bfd31a
Maneesha Netthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-01-19 11:12:30 +0530

    LinearEquation

---
+++ SolveLinearEquation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SolveLinearEquation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int x = sc.nextInt();
+
+        int y = a * x + b;
+
+        System.out.println(y);
+    } 
+}
