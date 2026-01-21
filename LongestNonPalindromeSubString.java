0b6a80138e24c1f44524624235a15395feca9592
Maneesha Netthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-01-22 00:05:53 +0530

    non palindrome

---
+++ LongestNonPalindromeSubString.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LongestNonPalindromeSubString {
+    static boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        if (!isPalindrome(s)) {
+            
+            System.out.println(s);
+        } else {
+            
+            System.out.println(s.substring(0, s.length() - 1));
+        }
+    }
+    
+}
