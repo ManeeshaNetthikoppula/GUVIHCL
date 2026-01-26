e194964525f98ee1eb0cd30ef201fae4556dcb74
Maneesha Netthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-01-26 22:06:24 +0530

    ..

---
+++ ReverseEachWord.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseEachWord {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            result.append(new StringBuilder(words[i]).reverse());
+            if (i != words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        System.out.println(result.toString());
+        sc.close();
+    }
+}
+
+    
