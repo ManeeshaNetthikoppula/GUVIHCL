cf43fcd183f2fbd85b0ad6ea53233d747051bd4d
Maneesha Netthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-01-28 19:05:41 +0530

    Add files via upload

---
+++ FindtheSingleNum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FindtheSingleNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            result ^= x;  // XOR accumulates the single number
+        }
+
+        System.out.println(result);
+    }
+    
+}
