82f76764e7948057fc3b029a3a8f4f47981f791f
Maneesha Netthikoppula <167111545+ManeeshaNetthikoppula@users.noreply.github.com>
2026-02-02 23:04:28 +0530

    Add files via upload

---
+++ RemoveDuplicateChar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RemoveDuplicateChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        String P = sc.next();
+        sc.close();
+
+        if (S.contains(P)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    } 
+    
+}
